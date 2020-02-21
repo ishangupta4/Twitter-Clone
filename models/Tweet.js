d4af65aa7cac7915f9942a33fba9ef8daf4e4e05
Ishan Gupta <ishangupta004@gmail.com>
2020-02-21 23:58:21 +0530

    Tweet model added

---
+++ models/Tweet.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TweetSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  username: {
+      type: String
+  },
+  text: {
+    type: String
+  },
+  mediaLinks: {
+    type: String
+  },
+  mentions: [
+    {
+      username: {
+          type: String
+      }
+    }
+  ],
+  hashtags: [
+    {
+        type: String
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  retweets: [
+      {
+          user: {
+              type: Schema.Types.ObjectId,
+              ref: 'users'
+          }
+      }
+  ],
+  replyingTo: [
+      {
+          username: {
+              type: String
+          }
+      }
+  ],
+  comments: [
+    {
+      tweet: {
+          type: Schema.Types.ObjectId,
+          ref: 'tweets'
+      }
+    }
+  ]
+});
+
+module.exports = Tweet = mongoose.model('tweets', TweetSchema);
